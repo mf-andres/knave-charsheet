87ba9825dcb6b4faefce8cc30385d1e1db1dbab7
mf-andres <amunoz@gradiant.org>
2024-04-04 11:05:34 +0200

    feat: add basic bonuses and defenses form and a button to roll initial bonuses

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,102 @@
-import Image from "next/image";
+"use client";
+
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
+  const [str, setStr] = useState(0);
+  const [dex, setDex] = useState(0);
+  const [con, setCon] = useState(0);
+  const [int, setInt] = useState(0);
+  const [wis, setWis] = useState(0);
+  const [cha, setCha] = useState(0);
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    setter: Function
+  ) => {
+    const newValue = parseInt(e.target.value);
+    setter(newValue);
+  };
+
+  const setStats = () => {
+    setStr(rollStat());
+    setDex(rollStat());
+    setCon(rollStat());
+    setInt(rollStat());
+    setWis(rollStat());
+    setCha(rollStat());
+  };
+
+  const rollStat = () => {
+    return Math.min(rollD(6), rollD(6), rollD(6));
+  };
+
+  const rollD = (n: number) => {
+    return Math.floor(Math.random() * n) + 1;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <h1>Character Stats</h1>
+      <form>
+        <label>Strength: </label>
+        <input
+          type="number"
+          value={str}
+          onChange={(e) => handleInputChange(e, setStr)}
         />
-      </div>
+        <br />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <label>Dexterity: </label>
+        <input
+          type="number"
+          value={dex}
+          onChange={(e) => handleInputChange(e, setDex)}
+        />
+        <br />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <label>Constitution: </label>
+        <input
+          type="number"
+          value={con}
+          onChange={(e) => handleInputChange(e, setCon)}
+        />
+        <br />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <label>Intelligence: </label>
+        <input
+          type="number"
+          value={int}
+          onChange={(e) => handleInputChange(e, setInt)}
+        />
+        <br />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <label>Wisdom: </label>
+        <input
+          type="number"
+          value={wis}
+          onChange={(e) => handleInputChange(e, setWis)}
+        />
+        <br />
+
+        <label>Charisma: </label>
+        <input
+          type="number"
+          value={cha}
+          onChange={(e) => handleInputChange(e, setCha)}
+        />
+        <br />
+      </form>
+
+      <h2>Calculated Stats</h2>
+      <p>Strength Defense: {10 + str}</p>
+      <p>Dexterity Defense: {10 + dex}</p>
+      <p>Constitution Defense: {10 + con}</p>
+      <p>Intelligence Defense: {10 + int}</p>
+      <p>Wisdom Defense: {10 + wis}</p>
+      <p>Charisma Defense: {10 + cha}</p>
+
+      <button onClick={(e) => setStats()}>Roll Stats</button>
+    </div>
   );
 }
