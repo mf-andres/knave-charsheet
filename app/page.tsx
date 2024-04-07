fe1d79c4b5c667e9b93daf88d5fae5a24bd80e10
mf-andres <amunoz@gradiant.org>
2024-04-07 21:02:46 +0200

    chore: work on presentation

--- app/page.tsx
+++ app/page.tsx
@@ -37,9 +37,60 @@ export default function Home() {
   };
 
   return (
-    <div className="flex">
-      <div>
-        <div className="flex-col bg-gray-300">
+    <div className="grid grid-cols-1 gap-3 bg-yellow-200">
+      <div className="col-start-1 col-span-6 row-start-1 row-span-1">
+        <div className="flex items-center m-1 bg-yellow-950 text-yellow-200 rounded">
+          <div className="p-2">Name:</div>
+          <input
+            type="text"
+            className="grow m-2 p-2 text-yellow-900 rounded"
+          ></input>
+        </div>
+      </div>
+
+      {/* TODO refactor level xp, hp and ac are similar looking input */}
+      <div className="col-start-7 col-span-1 row-start-1 row-span-1">
+        <div className="flex items-center m-1">
+          <div className="p-2">Level: </div>
+          <input
+            type="text"
+            className="w-16 m-2 p-2 text-yellow-900 rounded"
+          ></input>
+        </div>
+      </div>
+
+      <div className="col-start-8 col-span-1 row-start-1 row-span-1">
+        <div className="flex items-center m-1">
+          <div className="p-2 m-1">XP: </div>
+          <input
+            type="text"
+            className="w-16 m-2 p-2 text-yellow-900 rounded"
+          ></input>
+        </div>
+      </div>
+
+      <div className="col-start-7 col-span-1 row-start-2 row-span-1">
+        <div className="flex items-center m-1">
+          <div className="p-2">HP: </div>
+          <input
+            type="text"
+            className="w-16 m-2 p-2 text-yellow-900 rounded"
+          ></input>
+        </div>
+      </div>
+
+      <div className="col-start-8 col-span-1 row-start-2 row-span-1">
+        <div className="flex items-center m-1">
+          <div className="p-2">AC: </div>
+          <input
+            type="text"
+            className="w-16 m-2 p-2 text-yellow-900 rounded"
+          ></input>
+        </div>
+      </div>
+
+      <div className="col-start-1 col-span-6 row-start-2 row-span-1">
+        <div className="">
           <h1>Character Stats</h1>
           <form>
             <label>Strength: </label>
@@ -109,7 +160,16 @@ export default function Home() {
         </div>
       </div>
 
-      <Inventory></Inventory>
+      <div
+        id="inventory"
+        className="col-start-5 col-span-4 row-start-3 row-span-1"
+      >
+        <Inventory></Inventory>
+      </div>
+
+      <div className="col-start-1 col-span-4 row-start-3 row-span-1">
+        Traits
+      </div>
     </div>
   );
 }
